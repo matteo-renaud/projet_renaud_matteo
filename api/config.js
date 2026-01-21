c4629c5e6fceeedcd052285ccd92747e59549121
Mattéo RENAUD <mrenaud2@cfai-formation.fr>
2026-01-21 19:03:07 +0100

    chore: configuration BDD et fichiers pour déploiement sur render

--- api/config.js
+++ api/config.js
@@ -1,10 +1,10 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d59b2t15pdvs73a9r3eg-a.oregon-postgres.render.com",
+    "host" :"dpg-d5ogvvd6ubrc73f8pbg0-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "pollution_wo8h_user",
-    "password" : "M00vhjz7OVwjuYzcbzMuVKEwy05Am0IC",
-    "bdname" :"pollution_wo8h"
+    "user" : "pollution_fxhr_user",
+    "password" : "Nr1kZwUm1tn80qFWFMGnuC1Oy4GKe04P",
+    "bdname" :"pollution_fxhr"
     }
 }
